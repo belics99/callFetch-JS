9f4957e109167e04aa2025a4a36444b8becd08bf
Strahinja Belic <belicg22@gmail.com>
2020-03-10 13:51:37 +0100

    Update callFetch.js

--- callFetch.js
+++ callFetch.js
@@ -1,10 +1,20 @@
  /*
  default options:
+ ================
  options = {
  	method : 'POST',
  	header : {'Content-Type': 'application/json;charset=utf-8'},
  	body : JSON.stringify(data)
  }
+ 
+ PARAMS:
+ ========================
+ URL, DATA, FUNC, OPTIONS
+ ------------------------
+ URL     - endpoint
+ DATA    - data for endpoint
+ FUNC    - callback function 
+ OPTIONS - options for fetch
  */
  callFetch = async ( {url= '', data = {}, func = (result)=>{console.log(result)}, options = {method : 'POST', header : {'Content-Type': 'application/json;charset=utf-8'}, body : JSON.stringify(data)}} )=>{
     const opt = options
