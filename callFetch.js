06d18765918d141a609f4e65a9732c8208500e29
Strahinja Belic <belicg22@gmail.com>
2020-03-18 01:18:16 +0100

    Update callFetch.js

--- callFetch.js
+++ callFetch.js
@@ -18,7 +18,7 @@
  */
  callFetch = async ( {url= '', data = {}, func = (result)=>{console.log(result)}, options = {method : 'POST', header : {'Content-Type': 'application/json;charset=utf-8'}, body : JSON.stringify(data)}} )=>{
     const opt = options
-    let response = await fetch(url, options)
+    let response = await fetch(url, opt)
     let result = await response.json()
     func(result)        
 }
